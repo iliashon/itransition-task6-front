375c078a3b10a4cb16ab61c188ce7fd3b89b233d
Tupalski Ilya <itupalski@gmail.com>
2024-02-24 15:31:47 +0400

    feat: add meta secure (Sat Feb 24 15:31:47 +04 2024)

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,18 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+    async headers() {
+        return [
+            {
+                source: '/*',
+                headers: [
+                    {
+                        key: 'Content-Security-Policy',
+                        value: "upgrade-insecure-requests"
+                    }
+                ],
+            },
+        ]
+    },
     reactStrictMode: false,
 };
 
