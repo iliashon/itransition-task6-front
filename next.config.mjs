bfdc11b1e992a5548143394cfc4f1657deeb0384
Tupalski Ilya <itupalski@gmail.com>
2024-02-24 15:37:18 +0400

    fix: delete meta (Sat Feb 24 15:37:18 +04 2024)

--- next.config.mjs
+++ next.config.mjs
@@ -1,18 +1,5 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    async headers() {
-        return [
-            {
-                source: '/:path*',
-                headers: [
-                    {
-                        key: 'Content-Security-Policy',
-                        value: "upgrade-insecure-requests"
-                    }
-                ],
-            },
-        ]
-    },
     reactStrictMode: false,
 };
 
